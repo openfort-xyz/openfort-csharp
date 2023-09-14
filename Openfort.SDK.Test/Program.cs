8ed9bdc5acc83addc6ffcf16c9143e3deafc0d24
Nick Tsviatkov <nick.tsviatkov@gmail.com>
2023-09-14 17:49:43 +0200

    Update test script

--- Openfort.SDK.Test/Program.cs
+++ Openfort.SDK.Test/Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Openfort.SDK.Model;
+﻿using Openfort.SDK.Model;
 
-Console.WriteLine("Hello, World!");
-
-var client = new Openfort.SDK.OpenfortClient("sk_test_d4d75b24-2205-5ff7-8e71-076e83945ba8");
+var client = new Openfort.SDK.OpenfortClient("sk_test_XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX");
 var players = await client.Players.List();
 Console.WriteLine(players.Data.Count);
 
-var request = new AccountListRequest("pla_e8c6a205-ecf1-4ba4-861b-f8b2dbda06c3");
-var accounts = await client.Accounts.List(request);
-Console.WriteLine(accounts.Data.Count);
+if (players.Data.Count > 0)
+{
+    var request = new AccountListRequest(players.Data[0].Id);
+    var accounts = await client.Accounts.List(request);
+    Console.WriteLine(accounts.Data.Count);
+}
