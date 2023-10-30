79231e24b6f554027ddd8364259dae61efa351b2
Nick Tsviatkov <nick.tsviatkov@gmail.com>
2023-10-30 14:06:49 +0100

    OF-292 Add type of the event to webfook payload

--- Openfort.SDK/Model/WebHookEvent.cs
+++ Openfort.SDK/Model/WebHookEvent.cs
@@ -2,14 +2,17 @@
 {
 	public class WebHookEvent
 	{
-		public WebHookEvent(TransactionIntent data, int date)
+		public WebHookEvent(TransactionIntent data, int date, WebHookEventType type)
         {
 			Data = data;
 			Date = date;
+			Type = type;
 		}
 
         public TransactionIntent Data { get; set; }
 
         public int Date { get; set; }
-	}
+
+		public WebHookEventType Type { get; set; }
+    }
 }
